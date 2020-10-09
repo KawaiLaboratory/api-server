1ef06b246e1d793a778808ad06bdfcca8a9f9324
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-10-09 18:53:13 +0900

    既存のメアドを入力した際のHTTPステータスコード変更

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -1,6 +1,16 @@
 module Auth
   class RegistrationsController < DeviseTokenAuth::RegistrationsController
 
+    def create
+      user = User.find_by(email: sign_up_params[:email])
+      if user.present?
+        tmp_user = User.new(sign_up_params)
+        render json: {"status": :error, "data": tmp_user}, status: :conflict
+      else
+        super
+      end
+    end
+
   private
 
     def sign_up_params
