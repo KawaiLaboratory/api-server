5495b459ac2b946142831af55059a5b7bdc8b158
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-10-19 11:23:03 +0900

    タイポ修正

--- db/migrate/20200929040553_create_logs.rb
+++ db/migrate/20200929040553_create_logs.rb
@@ -2,7 +2,7 @@ class CreateLogs < ActiveRecord::Migration[5.2]
   def change
     create_table :logs do |t|
       t.references :user, foreign_key: true
-      t.string :detected_ble_tokenus, null: false
+      t.string :detected_ble_token, null: false
       t.integer :rssi, null: false
       t.integer :txpower, null: false
 
